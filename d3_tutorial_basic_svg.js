782dbbe3875f5f0c4e2684d9ba8cd1f93e8a1417
Sneha Narasimhan <email": null>
2016-06-20 19:08:49 -0700

    Add practice.

---
+++ d3_tutorial_basic_svg.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(e) { 
+  // Step 1: Create a canvas to draw the svg on.
+  var canvas = d3.select("body") // We use the body as a selector, but virtually any div can be used.
+                  .append("svg")
+                  .attr("width",500)
+                  .attr("height",500)
+
+  // Step 2: Add a circle to the canvas
+  var circle = canvas.append("circle")
+                      .attr("cx",250) // c = center | x = horizontal axis => Similar to margin in css
+                      .attr("cy",250)
+                      .attr("r",50) // Give the circle a radius
+                      .attr("fill","green") // Fill color of the object is an attribute, not a style
+
+});
